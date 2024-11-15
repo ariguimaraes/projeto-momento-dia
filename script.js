faa5538078389b6abf1457ae780859b4d1f5cf61
Ari Guimarães <146673812+ariguimaraes@users.noreply.github.com>
2024-11-15 15:53:35 -0300

    criado projeto-momento-dia

---
+++ script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/foto-manha.jpg'
+        document.body.style.background = '#4696f1'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/foto-tarde.jpg'
+        document.body.style.background = '#f6a609'
+    } else {
+        img.src = 'imagens/foto-noite.jpg'
+        document.body.style.background = '#2b2e4a'
+    }
+}
